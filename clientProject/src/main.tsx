84a580eaa132745953c78d9b41f72ff696115337
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2026-03-04 11:49:20 +0545

    Project setput

---
+++ clientProject/src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import LoginPage from './page/auth/login.page'
+import "./assets/css/global.css"
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+   <LoginPage/>
+  </StrictMode>,
+)
